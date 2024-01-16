95885ea84ce68309d564b2f91aff7ecd012ed5f9
Theodore Jones <35896902+theopjones@users.noreply.github.com>
2024-01-15 21:19:30 -0500

    Formatting Fix on Home Page

--- template/scripts/listings.py
+++ template/scripts/listings.py
@@ -11,11 +11,14 @@ def generate_content(params):
     html_output = '<section class="re-listings-section">\n<div class="re-listings-grid">\n'
     for feature in data["features"][:num_listings]:
         properties = feature["properties"]
+        formatted_price = "{:,}".format(properties["listPrice"])
+        mls_id = properties["mlsId"]
         html_output += f'<div class="re-listing-item">\n'
         html_output += f'<img src="{properties["listingPhoto"]}" alt="Listing Photo">\n'
         html_output += f'<p>{properties["fullAddress"]}</p>\n'
         html_output += f'<p>Bedrooms: {properties["bedrooms"]}, Bathrooms: {properties["bathrooms"]}, Area: {properties["area"]} sqft</p>\n'
-        html_output += f'<p class="re-listing-price">Price: ${properties["listPrice"]}</p>\n'
+        html_output += f'<p class="re-listing-price">Price: ${formatted_price}</p>\n'
+        html_output += f'<a href="/listing/listing_{mls_id}.html">More Information</a>\n'
         html_output += '</div>\n'
     html_output += '</div>\n</section>'
 
